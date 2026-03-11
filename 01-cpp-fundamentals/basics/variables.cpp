95292f055fff9aa679e27fcd4f49f76c7cd95336
Shreya <pshrey0430@gmail.com>
2026-03-11 17:52:11 +0530

    Variables example add

---
+++ 01-cpp-fundamentals/basics/variables.cpp
@@ -0,0 +1,16 @@
+//Example code that shows declaration and initialization of different types pf variables
+
+#include<iostream>
+
+int main(){
+
+    int roll_no; //variable declaration
+    roll_no = 23; //variable initialization
+
+    char group = 'A'; //char variable declaration and initialization
+    //when declaring char type always do it inside ''
+
+    float price = 12.75f; //after float you should write f, otherwise by default it will consider double
+    
+    return 0;
+}
