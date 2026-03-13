e7f9dc93a349a6de1233cb79364e5680774e196d
Shreya <pshrey0430@gmail.com>
2026-03-13 07:55:01 +0530

    Days of week program to show else if ladder

---
+++ 01-cpp-fundamentals/2_conditional_statements/day_of_week.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+int main(){
+
+    int day_no;
+
+    std::cout<<"Enter the day number: ";
+    std::cin>>day_no;
+
+    if(day_no == 1){
+        std::cout<<"Monday"<<std::endl;
+    }
+    else if(day_no == 2){
+        std::cout<<"Tuesday"<<std::endl;
+    }
+    else if(day_no == 3){
+        std::cout<<"Wednesday"<<std::endl;
+    }
+    else if(day_no == 4){
+        std::cout<<"Thursday"<<std::endl;
+    }
+    else if(day_no == 5){
+        std::cout<<"Friday"<<std::endl;
+    }
+    else if(day_no == 6){
+        std::cout<<"Saturday"<<std::endl;
+    }
+    else if(day_no == 7){
+        std::cout<<"Sunday"<<std::endl;
+    }
+    else{
+        std::cout<<"Invalid Day"<<std::endl;
+    }
+    
+    return 0;
+}
