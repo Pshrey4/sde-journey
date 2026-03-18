436d7c49de1a75a250569153cd7da339e781572c
Shreya <pshrey0430@gmail.com>
2026-03-18 12:00:38 +0530

    Updated pattern type 3 with one more variation of for loop

--- 01-cpp-fundamentals/4_arrays/pattern_type_3.cpp
+++ 01-cpp-fundamentals/4_arrays/pattern_type_3.cpp
@@ -6,8 +6,17 @@ int main(){
     std::cout<<"Enter a number: ";
     std::cin>>num;
 
+    //variation 1
     for(int i=0; i<num; i++){
         for(int j=num; j>i; j--){
+            std::cout<<"* ";
+        }
+        std::cout<<"\n";
+    }
+    
+    //variation 2
+    for(int i=num; i>0; i--){
+        for(int j=0; j<i; j++){
             std::cout<<"* ";
         }
         std::cout<<"\n";
