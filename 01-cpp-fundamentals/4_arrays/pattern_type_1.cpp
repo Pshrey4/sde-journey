448529d2717dd81edde7f396968afc9723cd512e
Shreya <pshrey0430@gmail.com>
2026-03-18 11:40:39 +0530

    Pattern program type 1

---
+++ 01-cpp-fundamentals/4_arrays/pattern_type_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(){
+    int num;
+
+    std::cout<<"Enter a number: ";
+    std::cin>>num;
+
+    for(int i=0; i<num; i++){
+        for(int j=0; j<num; j++){
+            std::cout<<"* ";
+        }
+        std::cout<<"\n";
+    }
+
+    return 0;
+}
