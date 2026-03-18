cc343fa87f1f1c1e3935a8e88f263a3446489bb1
Shreya <pshrey0430@gmail.com>
2026-03-18 10:28:13 +0530

    Program to find max element in array

---
+++ 01-cpp-fundamentals/4_arrays/max_element.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+    int arr[] = {2, 4, 3, 6, 8, 7, 5, 9}, max = 0;
+
+    for(auto i: arr){
+        if(i>max){
+            max = i;
+        }
+    }
+
+    std::cout<<"Maximum element of array: "<<max<<std::endl;
+
+    return 0;
+}
