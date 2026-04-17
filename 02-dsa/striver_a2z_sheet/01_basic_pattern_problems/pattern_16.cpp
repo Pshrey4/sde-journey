c9986b1e4fd6a9623f3c9c01c3845d8f5409601b
Shreya <pshrey0430@gmail.com>
2026-04-17 15:47:12 +0530

    Basic pattern problem 16

---
+++ 02-dsa/striver_a2z_sheet/01_basic_pattern_problems/pattern_16.cpp
@@ -0,0 +1,38 @@
+// Problem Statement: Given an integer n, print the following output
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E 
+
+// Approach:
+// Run an outer loop (i) from 0 to n-1 to handle rows.
+// Take char variable ch store 'A'+i
+// Run an inner loop (j) from 0 to i
+// Inside inner loop print ch
+
+// Complexity Analysis:
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        char ch = 'A'+i;
+
+        for(int j=0; j<=i; j++){  
+            cout<<ch<<" ";
+        }  
+
+        cout<<endl;
+    }
+
+    return 0;
+}
