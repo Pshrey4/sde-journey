d198564850c6f88af567974d6f5dc59b653b7132
Shreya <pshrey0430@gmail.com>
2026-05-11 18:03:55 +0530

    Symmetric Butterfly Pattern

---
+++ 02-dsa/striver_a2z_sheet/01_basic_pattern_problems/20_symmetric_butterfly.cpp
@@ -0,0 +1,78 @@
+// Problem Statement: Given an integer n, print the following output
+// *         *
+// * *     * *
+// * * * * * *
+// * *     * *
+// *         *
+
+// Approach:
+// Upper Half: Crown
+// Iterate from i = 1 to i<=n.
+// Inner loop prints i stars on the left side.
+// Another loop prints 2*(n-i) spaces in the middle.
+// Next loop prints i stars on the right side.
+// Print a newline after completing each row.
+// Lower Half: Inverted Crown
+// Iterate from i = n-1 to i>0.
+// Inner loop prints i stars on the left side.
+// Another loop prints 2*(n-i) spaces in the middle.
+// Next loop prints i stars on the right side.
+// Print a newline after completing each row.
+
+// Complexity Analysis:
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    //loop to print upper half crown
+    for(int i=1; i<=n; i++){
+
+        //loop to print stars on left
+        for(int j=1; j<=i; j++){  
+            cout<<"* ";
+        } 
+        
+        //loop to print spaces
+        for(int j=1; j<=2*(n-i); j++){
+            cout<<"  ";
+        }
+
+        //loop to print stars on right
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+
+    //loop to print lower half inverted crown
+    for(int i=n-1; i>0; i--){
+
+        //loop to print stars on left
+        for(int j=1; j<=i; j++){  
+            cout<<"* ";
+        } 
+        
+        //loop to print spaces
+        for(int j=1; j<=2*(n-i); j++){
+            cout<<"  ";
+        }
+
+        //loop to print stars on right
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
