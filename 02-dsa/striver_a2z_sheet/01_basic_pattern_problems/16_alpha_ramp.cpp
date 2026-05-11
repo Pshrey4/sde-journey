3a84d46473da6568d756e790303b7bcab3c25092
Shreya <pshrey0430@gmail.com>
2026-05-11 16:28:19 +0530

    Alpha Ramp Pattern

--- 02-dsa/striver_a2z_sheet/01_basic_pattern_problems/16_alpha_ramp.cpp
+++ 02-dsa/striver_a2z_sheet/01_basic_pattern_problems/16_alpha_ramp.cpp
@@ -25,12 +25,11 @@ int main(){
     cin>>n;
 
     for(int i=0; i<n; i++){
-        char ch = 'A'+i;
 
         for(int j=0; j<=i; j++){  
-            cout<<ch<<" ";
+            cout<<char('A'+i)<<" ";
         }  
-
+        
         cout<<endl;
     }
 
